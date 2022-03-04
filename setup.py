f1694c28c2e684fe78ff34def91b18f58cbe2630
Jennings Zhang <jenni_zh@protonmail.com>
2022-03-03 23:18:20 -0500

    v0.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='pycivet',
-    version='0.0.2a2',
+    version='0.0.3',
     description='Object-oriented CIVET bindings for Python',
     long_description=readme,
     long_description_content_type='text/markdown',
